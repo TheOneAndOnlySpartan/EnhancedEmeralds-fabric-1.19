93db3c2f70c4e60b86d35725dca93a5b49fc587d
TheOneAndOnlySpartan <oneandonlyspartan@outlook.com>
2022-07-16 13:05:13 +0500

    Uh, commit :)

---
+++ src/main/java/net/dinnerchief/enhancedemeralds/EnhancedEmeraldsClient.java
@@ -0,0 +1,10 @@
+package net.dinnerchief.enhancedemeralds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnhancedEmeraldsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dinnerchief/enhancedemeralds/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.dinnerchief.enhancedemeralds.mixin;
+
+import net.dinnerchief.enhancedemeralds.EnhancedEmeralds;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		EnhancedEmeralds.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}

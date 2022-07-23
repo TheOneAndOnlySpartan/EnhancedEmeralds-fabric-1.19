497e00cbc6432f794e9ede5ed98d3f88e7c9166f
TheOneAndOnlySpartan <TheSpartanFromMicrosoft>
2022-07-23 21:32:47 +0500

    Removed Solid Orbs and instead made 3 tier exp chunks and added charged emerald that looks like a normal emerald (don't know how to make it glow)

--- src/main/java/net/dinnerchief/enhanced_emeralds/item/ModItems.java
+++ src/main/java/net/dinnerchief/enhanced_emeralds/item/ModItems.java
@@ -3,20 +3,29 @@ package net.dinnerchief.enhanced_emeralds.item;
 import net.dinnerchief.enhanced_emeralds.EnhancedEmeralds;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item SOLID_EXPERIENCE_ORB = registerItem("solid_experience_orb",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    // INITIALIZING ITEMS
+    public static final Item SMALL_EXP_CHUNK = registerItem("small_experience_chunk", new Item(new FabricItemSettings().group(ModItemGroup.ENHANCED_EMERALDS)));
+    public static final Item MEDIUM_EXP_CHUNK = registerItem("medium_experience_chunk", new Item(new FabricItemSettings().group(ModItemGroup.ENHANCED_EMERALDS)));
+    public static final Item BIG_EXP_CHUNK = registerItem("big_experience_chunk", new Item(new FabricItemSettings().group(ModItemGroup.ENHANCED_EMERALDS)));
+    public static final Item CHARGED_EMERALD = registerItem("charged_emerald", new Item(
+            new FabricItemSettings()
+                    .maxCount(16)
+                    .rarity(Rarity.EPIC)
+                    .group(ModItemGroup.ENHANCED_EMERALDS)
+    ));
 
-
+    // REGISTERING FUNCTION
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(EnhancedEmeralds.MOD_ID, name), item);
     }
 
+    // HAVE TO BE HERE
     public static void registerModItems() {
         EnhancedEmeralds.LOGGER.debug("Registering Items for " + EnhancedEmeralds.MOD_ID);
     }

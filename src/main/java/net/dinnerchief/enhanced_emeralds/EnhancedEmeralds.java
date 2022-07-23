7795db18c5920d147dbec15206bf6ddd91caf1cc
TheOneAndOnlySpartan <TheSpartanFromMicrosoft>
2022-07-23 21:35:30 +0500

    Minor Changed

--- src/main/java/net/dinnerchief/enhanced_emeralds/EnhancedEmeralds.java
+++ src/main/java/net/dinnerchief/enhanced_emeralds/EnhancedEmeralds.java
@@ -1,16 +1,21 @@
 package net.dinnerchief.enhanced_emeralds;
 
+import net.dinnerchief.enhanced_emeralds.block.ModBlocks;
 import net.dinnerchief.enhanced_emeralds.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class EnhancedEmeralds implements ModInitializer {
 	public static final String MOD_ID = "enhanced_emeralds";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModItems();
 	}
 }
--- src/main/java/net/dinnerchief/enhanced_emeralds/mixin/ExampleMixin.java
+++ src/main/java/net/dinnerchief/enhanced_emeralds/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		EnhancedEmeralds.LOGGER.info("This line is printed by an example mod mixin!");
+		EnhancedEmeralds.LOGGER.info("Enhanced Emeralds");
 	}
 }
